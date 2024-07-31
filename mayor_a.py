8eb89d103c44f3500a16ef996b49e04472d34fd8
Almendras <pittietoon@gmail.com>
2024-07-31 19:26:53 -0400

    se agregan comentarios a primeros auxilios y se crea Readme

---
+++ mayor_a.py
@@ -0,0 +1,27 @@
+#Librerias
+import sys
+
+#Diccionario de meses con sus valores
+ventas = {
+"Enero": 15000,
+"Febrero": 22000,
+"Marzo": 12000,
+"Abril": 17000,
+"Mayo": 81000,
+"Junio": 13000,
+"Julio": 21000,
+"Agosto": 41200,
+"Septiembre": 25000,
+"Octubre": 21500,
+"Noviembre": 91000,
+"Diciembre": 21000,
+}
+
+buscar = int(sys.argv[1])
+
+#Sin comprehensions
+mayor_a = {}
+for mes, valor in ventas.items():
+    if valor >= buscar:
+        mayor_a[mes] = valor
+print(mayor_a)
---
+++ primeros_auxilios.py
@@ -0,0 +1,23 @@
+#Ingresa consulta de estimulos del paciente
+responde_a_estimulos = input("¿Responde a estímulos?: ").lower() == "si"
+#consulta responde a los estimulos
+if responde_a_estimulos:
+    print("Valorar la necesidad de llevar al hospital más cercano")
+else:
+    print("Abrir la vía aérea")
+    respira = input("¿Respira?: ").lower() == "si"
+    if respira:
+        print("Permitir posición de suficiente ventilación")
+    else:
+        print("Administrar 5 ventilaciones y llamar a una ambulancia")
+# consulta por signos vitales del paciente
+        while True:
+            signos_vitales = input("¿Hay signos vitales?: ").lower() == "si"
+            if signos_vitales:
+                print("Reevaluar a la espera de la ambulancia")
+                llego_ambulancia = input("¿Llegó la ambulancia?: ").lower() == "si"
+                if llego_ambulancia:
+                    print("Se lleva al paciente")
+                    break  # salir del bucle si llegó la ambulancia
+            else:
+                print("Administrar compresiones torácicas hasta que llegue la ambulancia")
